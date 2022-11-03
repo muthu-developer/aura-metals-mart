089b269734302ab5d81f0385cef1edf9e03c3502
Muthulingaraj@96 <muthulingaraj96@gmail.com>
2022-11-04 00:15:47 +0530

    header-component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Shop from "./assets/images/store.png";
 import CS from "./assets/images/coming-soon.png"
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <div className='home-page'>
         <div className='logo-section'>
           <img src={Shop} alt="Aura Metals Mart" className='logo' />
